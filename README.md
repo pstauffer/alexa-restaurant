e4f6a56894e81debb17c29772293c3a9dffc39f6
pstauffer <pstauffer@confirm.ch>
2017-03-03 01:33:34 +0100

    REFACTOR: made flask docker-compatible

--- README.md
+++ README.md
@@ -14,6 +14,37 @@ pip install -r requirements.txt
 python alexa-restaurant.py
 ```
 
-## Config
+## Configuration
 
-Change the `config.yml` to add your restaurants.
+### App
+If you like another listen_ip or port, set the values in the `config.yml`
+
+```
+app_port: 5555
+app_listen_ip: 0.0.0.0
+```
+
+### Customize Restaurants
+Create a `template.yml` file and add these key entries with your values.
+
+```
+---
+bye_msg: Okay schade... bye
+
+welcome_msg: Hallo Raiffeisen, soll ich euch sagen, wohin wir essen gehen?
+
+answer_msg: Wir gehen
+
+start_msg: Restaurant picker läuft
+
+restaurants:
+  - in den Schwanen
+  - ins Armenhaus
+  - in die Tschiardino
+  - zum Gas-Heini
+  - über die Gasse
+  - zur Taverna
+  - ins Gentile
+  - zum Bierfalken
+  - zum Veccia Posta
+```
--- app.py
+++ app.py
@@ -5,25 +5,35 @@ import sys
 from random import randint
 
 
+def yml_loader(file):
+    try:
+        with open(file, 'r') as stream:
+            return yaml.load(stream)
+    except IOError:
+        sys.exit(1)
+
+
+template = yml_loader('template.yml')
+
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 
 def get_restaurant():
-    restaurants = config['restaurants']
+    restaurants = template['restaurants']
     rand = randint(0, len(restaurants) - 1)
     return restaurants[rand]
 
 
 @app.route('/')
 def homepage():
-    homepage = config['homepage']
+    homepage = template['start_msg']
     return homepage
 
 
 @ask.launch
 def start_skill():
-    welcome_msg = config['welcome']
+    welcome_msg = template['welcome_msg']
     return question(welcome_msg)
 
 
@@ -31,33 +41,26 @@ def start_skill():
 @ask.intent('YesIntent')
 def yes_intent():
     restaurant = get_restaurant()
-    return statement(config['answer'] + ' ' + restaurant)
+    return statement(template['answer_msg'] + ' ' + restaurant)
 
 
 @ask.intent('AMAZON.NoIntent')
 def no_intent():
-    bye_text = config['bye']
+    bye_text = template['bye_msg']
     return statement(bye_text)
 
 
 @ask.intent('AMAZON.StopIntent')
 def stop_intent():
-    bye_text = config['bye']
+    bye_text = template['bye_msg']
     return statement(bye_text)
 
 
 @ask.intent('AMAZON.CancelIntent')
 def cancel_intent():
-    bye_text = config['bye']
+    bye_text = template['bye_msg']
     return statement(bye_text)
 
 
 if __name__ == '__main__':
-
-    try:
-        with open('config.yml', 'r') as stream:
-            config = yaml.load(stream)
-    except IOError as err:
-        sys.exit(1)
-
-    app.run(debug=True, host=config['app_listen_ip'], port=config['app_port'])
+    app.run(debug=True)
